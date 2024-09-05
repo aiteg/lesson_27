2e9db7cf6cbf97e1166ccc8d67756ae4a07d6dbe
aiteginbaratov <redmifly@gmail.com>
2024-09-05 19:43:38 +0600

    Commit in new branch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         System.out.println(a);
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("HEllL WORLD");
+        }
     }
 }
